1bef138f48d790c3db46fe497ea6024264916ab7
Kavitha <you@example.com>
2025-03-30 14:50:13 +0530

    BasicPrint

---
+++ src/Java_Basics/HelloWorld.java
@@ -0,0 +1,7 @@
+package Java_Basics;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hi Kavitha!Welcome to Automation Testing Learning!");
+    }
+}
