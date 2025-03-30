42db721dfacc94e20a661d6c6c48c3ad0a9e7619
Kavitha <you@example.com>
2025-03-30 17:08:58 +0530

    Table 9 using printf

---
+++ src/Java_Basics/Table9.java
@@ -0,0 +1,20 @@
+package Java_Basics;
+
+public class Table9 {
+    public static void main(String[] args) {
+
+        for(int i=1;i<=10;i++)
+        {
+            int k=i*9;
+            //System.out.println(i+"*9="+k);
+
+            System.out.printf(i+"*9="+"%d%n",+k);
+
+        }
+
+
+
+
+
+    }
+}
